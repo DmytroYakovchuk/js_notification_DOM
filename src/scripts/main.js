ca6485a669ef9887a987889358776d53e825f828
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-07-28 16:19:58 +0300

    three border with information

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,30 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification', type);
+
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  const elTitle = document.createElement('h2');
+
+  elTitle.classList.add('title');
+  elTitle.textContent = title;
+
+  const elDescription = document.createElement('p');
+
+  elDescription.textContent = description;
+
+  notification.appendChild(elTitle);
+  notification.appendChild(elDescription);
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
